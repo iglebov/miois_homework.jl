f3bd1caea0924a23c45d4225fc28d2bc30f84af5
Ivan Glebov <iglebov40@gmail.com>
2025-11-04 12:27:06 +0300

    Solve task01 and task02

--- src/task01.jl
+++ src/task01.jl
@@ -1,34 +1,103 @@
 export getdigits, touppercase, firstnorm, secondnorm, infnorm, isleap, chesscolor
 
 function getdigits(a)
-    return 0
+    temp = [];
+    b = a;
+
+    while (b / 10) > 1
+        push!(temp, b % 10);
+        b = b ÷ 10;
+    end
+
+    push!(temp, b);
+    return temp[end:-1:1];
 end
 
 function touppercase(str_)
-    return 0;
+    temp = [];
+
+    for char in str_
+
+        if (char >= 'a') && (char <= 'z')
+            push!(temp, char + ('A' - 'a'));
+        else
+            push!(temp, char);
+        end
+
+    end
+
+    return join(temp);
 end
 
 function firstnorm(vec_::AbstractVector{<:Number})
-    return 0;
+    sum = 0;
+
+    for number in vec_
+        sum += abs(number);
+    end
+
+    return sum;
 end
 
 function secondnorm(vec_::AbstractVector{<:Number})
-    return 0;
+    sum = 0;
+
+    for number in vec_
+        sum += number ^ 2;
+    end
+
+    return sqrt(sum);
 end
 
 function infnorm(vec_::AbstractVector{<:Number})
-    return 0;
+    sum = 0;
+
+    for number in vec_
+        if number > sum
+            sum = number;
+        end
+    end
+
+    return sum;
 end
 
-function firstnorm(vec_::AbstractMatrix{<:Number})
-    return 0;
+function firstnorm(mat_::AbstractMatrix{<:Number})
+    result = 0;
+
+    for column in eachcol(mat_)
+        temp = 0
+
+        for cell in column
+            temp += cell;
+        end
+
+        result = result < temp ? temp : result;
+    end
+
+    return result;
 end
 
-function infnorm(vec_::AbstractMatrix{<:Number})
-    return 0;
+function infnorm(mat_::AbstractMatrix{<:Number})
+    result = 0;
+
+    for column in eachrow(mat_)
+        temp = 0
+
+        for cell in column
+            temp += cell;
+        end
+
+        result = result < temp ? temp : result;
+    end
+
+    return result;
 end
 
 function isleap(year)
+    if (year % 400) == 0 || (year % 4) == 0
+        return true;
+    end
+
     return false;
 end
 
